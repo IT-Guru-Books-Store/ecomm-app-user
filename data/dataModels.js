b9be1d351cef0be19f7db54bf4c8cd691e7ff288
lakshankd <dominiclakshank@gmail.com>
2024-02-23 23:05:28 +0530

    changing img link.

--- data/dataModels.js
+++ data/dataModels.js
@@ -6,7 +6,7 @@ export const cartData = [
     bookDescription: "Est culpa ad reprehenderit ullamco dolor.",
     price: "599.00",
     quantity: 2,
-    img: "https://images.unsplash.com/photo-1589829085413-56de8ae18c73?q=80&w=3012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "https://images.unsplash.com/photo-1592496431122-2349e0fbc666?q=80&w=3012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     bookId: "363787ghdsd23",
@@ -15,6 +15,6 @@ export const cartData = [
     bookDescription: "Est culpa ad reprehenderit ullamco dolor.",
     price: "599.00",
     quantity: 1,
-    img: "https://images.unsplash.com/photo-1589829085413-56de8ae18c73?q=80&w=3012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "https://images.unsplash.com/photo-1592496431122-2349e0fbc666?q=80&w=3012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
