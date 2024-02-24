2943e565f01f88df2d79f9bdd5100d1e28839a42
lakshankd <dominiclakshank@gmail.com>
2024-02-24 13:59:05 +0530

    adding pages.

---
+++ app/wholesale-request/page.js
@@ -0,0 +1,12 @@
+import WholesaleRequestHeader from "@/components/wholesele-request/WholesaleRequestHeader";
+import React from "react";
+
+const WholesaleRequest = () => {
+  return (
+    <div>
+      <WholesaleRequestHeader />
+    </div>
+  );
+};
+
+export default WholesaleRequest;
