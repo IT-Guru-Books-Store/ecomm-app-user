4854c0b9705e95ac656be7978844c0de8a4183a4
salinda-yasas-99 <salindasym@gmail.com>
2024-03-02 01:11:12 +0530

    adding form

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,15 +1,71 @@
+import TextBox from "@/components/textBox/TextBox";
 import React from "react";
 
 const Checkout = () => {
   return (
-    <div className="w-full flex flex-col h-[90vh]">
+    <div className="w-full flex flex-col h-[100vh]">
       <h1 className="text-[22px] font-bold text-[#073763] mt-4 ml-3">
         Checkout
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-x-[50px]">
         {/* billing details */}
-        <div className="bg-red-500 px-3">
+        <div className="px-3">
           <h2 className="text-xl">Billing Details</h2>
+          {/* form */}
+          <form>
+            <div className="grid md:grid-cols-2 gap-x-[32px]">
+              <div>
+                <TextBox
+                  placeholder={"Enter Your First Name"}
+                  label={"First Name"}
+                />
+              </div>
+              <div>
+                <TextBox
+                  placeholder={"Enter Your Last Name"}
+                  label={"Last Name"}
+                />
+              </div>
+            </div>
+            <div>
+              <TextBox placeholder={"Enter Your Address"} label={"Address"} />
+            </div>
+            <div>
+              <TextBox placeholder={"Enter Your City"} label={"City"} />
+            </div>
+            <div>
+              <TextBox
+                placeholder={"Enter Postal Code"}
+                label={"Postal Code"}
+              />
+            </div>
+            <div>
+              <TextBox
+                placeholder={"Enter Your Mobile Number"}
+                label={"Mobile Number 1"}
+              />
+            </div>
+            <div>
+              <TextBox
+                placeholder={"Enter Another Mobile Number"}
+                label={"Mobile Number 2"}
+              />
+            </div>
+            <div>
+              <TextBox placeholder={"Enter Your Email"} label={"Email"} />
+            </div>
+            <div>
+              <label for="Order Notes" class="block mb-2 text-sm">
+                Order Notes
+              </label>
+              <textarea
+                id="Order Notes"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-600 placeholder-gray-400"
+                placeholder="Notes about your order, eg:- special notes for delivery"
+              />
+            </div>
+          </form>
         </div>
         {/* Your Order */}
         <div className="bg-gray-500 h-[50vh]">
---
+++ components/textBox/TextBox.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TextBox = (props) => {
+  return (
+    <div>
+      <label for={props.label} class="block mb-2 text-sm">
+        {props.label}
+      </label>
+      <input
+        type="text"
+        id={props.label}
+        class="bg-[#fff] border text-gray-900 text-sm rounded-lg block w-full p-2.5 border-gray-600 placeholder-gray-400"
+        placeholder={props.placeholder}
+        required
+      />
+    </div>
+  );
+};
+
+export default TextBox;
