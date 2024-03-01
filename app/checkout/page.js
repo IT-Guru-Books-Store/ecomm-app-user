02c30e49a9cb9ce712d83a037a574158b33ea57e
salinda-yasas-99 <salindasym@gmail.com>
2024-03-02 00:19:05 +0530

    initial commit

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <div className="w-full flex flex-col h-[90vh]">
+      <h1 className="text-[22px] font-bold text-[#073763] mt-4 ml-3">
+        Checkout
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full">
+        {/* billing details */}
+        <div className="bg-red-500 px-3">
+          <h2 className="text-xl">Billing Details</h2>
+        </div>
+        {/* Your Order */}
+        <div className="bg-gray-500 h-[50vh]">
+          <h2 className="text-xl">Your Order</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
