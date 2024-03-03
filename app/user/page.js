c5ee7fb360518dc4519d5df9719411a4712e0256
salinda-yasas-99 <salindasym@gmail.com>
2024-03-03 20:39:04 +0530

    initial commit

--- app/user/page.js
+++ app/user/page.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div className="flex flex-col items-center w-full bg-green-100 h-[800px] md:py-[2%]">
+      <div className="flex w-[100%] md:w-[70%] h-[800px] bg-yellow-200">
+        <h1>Profile</h1>
+      </div>
+    </div>
+  );
 };
 
 export default User;
