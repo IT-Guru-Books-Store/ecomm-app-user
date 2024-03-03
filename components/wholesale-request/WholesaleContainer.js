2c11f1ff43e1ae487304c973dfee1def4f21870e
lakshankd <dominiclakshank@gmail.com>
2024-03-03 22:26:03 +0530

    adding pages and comps to wholesale req.

---
+++ components/wholesale-request/WholesaleContainer.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import WholesaleOrderDetails from "./wholesale-content/WholesaleOrderDetails";
+import Approval from "./wholesale-content/Approval";
+import Confirmation from "./wholesale-content/Confirmation";
+
+const WholesaleContainer = () => {
+  const [currentStep, setCurrentState] = useState(1);
+  if (currentStep === 1) return <WholesaleOrderDetails />;
+  else if (currentStep === 2) return <Approval />;
+  else if (currentStep === 3) return <Confirmation />;
+  else return <div>No content: Error has occurred!!</div>;
+};
+
+export default WholesaleContainer;
